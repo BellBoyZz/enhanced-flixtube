2a3cf958977946f4513b09efdf9923a764aa302b
peerasu <peerasu.w@ku.th>
2023-02-25 16:10:37 +0700

    finish midterm

---
+++ fixtures/products/products.js
@@ -0,0 +1,29 @@
+const mongodb = require("mongodb");
+
+module.exports = [
+    {
+        _id: mongodb.ObjectId("63f9bd64b73bd29281ea7f8e"),
+        name: "Netflix",
+        url: 'https://www.netflix.com'
+    },
+    {
+        _id: mongodb.ObjectId("63f9bd89e76b9e29a0141f37"),
+        name: "Agoda",
+        url: 'https://www.agoda.com'
+    },
+    {
+        _id: mongodb.ObjectId("63f9bd97b49098942c2c63e9"),
+        name: "Lazada",
+        url: 'https://www.lazada.co.th/'
+    },
+    {
+        _id: mongodb.ObjectId("63f9bda3aa3956cfe2e99941"),
+        name: "Lineman",
+        url: 'https://lineman.line.me/'
+    },
+    {
+        _id: mongodb.ObjectId("63f9bdb553caf9a6fd763460"),
+        name: "Shopee",
+        url: 'https://www.netflix.co.th'
+    }
+];
